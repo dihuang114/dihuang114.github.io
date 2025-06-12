5085465cec5147d8b8412350a36ebdbfff0735dd
dihuang114 <dihuang2023@163.com>
2025-06-12 14:27:32 +0800

    Site updated: 2025-06-12 14:27:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/declare/","/mustsee/","/download/","2025/06/11/test/"];function toRandomPost(){
+var posts=["/declare/","/download/","/mustsee/","2025/06/11/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
