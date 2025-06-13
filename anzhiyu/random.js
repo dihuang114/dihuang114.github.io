19b492169d01dfabace767abd7144297fdea5ec5
dihuang114 <dihuang2023@163.com>
2025-06-13 23:35:02 +0800

    Site updated: 2025-06-13 23:34:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/declare/","/download/","/mustsee/","2025/06/11/test/","2025/06/13/杀戮尖塔/"];function toRandomPost(){
+var posts=["/declare/","/download/","/mustsee/","2025/06/11/test/","2025/06/13/slaythespire/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
