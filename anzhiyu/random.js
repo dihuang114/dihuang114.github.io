1e86f9b4583adb7f7273d0f4f509d9add5705482
dihuang114 <dihuang2023@163.com>
2025-06-15 20:59:16 +0800

    Site updated: 2025-06-15 20:59:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/declare/","/mustsee/","/download/","2025/06/13/slaythespire/"];function toRandomPost(){
+var posts=["/declare/","/mustsee/","/download/","2025/06/15/pcsoftware/bandizam/","2025/06/15/pcsoftware/bandizip/","2025/06/15/pcsoftware/everything/","2025/06/15/pcsoftware/newpc/","2025/06/15/pcsoftware/snipaste/","2025/06/15/pcsoftware/winrar/","2025/06/13/pcgame/slaythespire/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
