f1ede461bea193717a8ab74d04437c7723b8a821
dihuang114 <dihuang2023@163.com>
2025-06-16 10:03:57 +0800

    Site updated: 2025-06-16 10:03:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["/declare/","/mustsee/","/download/","2025/06/15/pcsoftware/bandizam/","2025/06/15/pcsoftware/bandizip/","2025/06/15/pcsoftware/everything/","2025/06/15/pcsoftware/newpc/","2025/06/15/pcsoftware/snipaste/","2025/06/15/pcsoftware/winrar/","2025/06/13/pcgame/slaythespire/","2025/06/15/pcsoftware/adobe2025/"];function toRandomPost(){
+var posts=["/declare/","/mustsee/","/download/","2025/06/15/pcsoftware/bandizam/","2025/06/15/pcsoftware/bandizip/","2025/06/15/pcsoftware/everything/","2025/06/15/pcsoftware/newpc/","2025/06/15/pcsoftware/snipaste/","2025/06/15/pcsoftware/winrar/","2025/06/13/pcgame/slaythespire/","2025/06/15/pcsoftware/adobe2025/","2025/06/16/web/computer/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
